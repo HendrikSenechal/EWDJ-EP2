de4cd735c5f6a0d924f18ab7c85c781b882f3d33
HendrikSenechal <114407384+HendrikSenechal@users.noreply.github.com>
2026-05-19 21:02:47 +0200

    Initialise Spring Project

---
+++ src/test/java/com/example/ewdj_ep3/EwdjEp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewdj_ep3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdjEp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

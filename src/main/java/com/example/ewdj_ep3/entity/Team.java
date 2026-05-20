1cf39107f3bf0a8e09e21a86e010b8b83b31bc49
HendrikSenechal <114407384+HendrikSenechal@users.noreply.github.com>
2026-05-20 10:47:34 +0200

    Jpa + Team entity + Persistence

--- src/main/java/com/example/ewdj_ep3/entity/Team.java
+++ src/main/java/com/example/ewdj_ep3/entity/Team.java
@@ -1,2 +1,20 @@
-package com.example.ewdj_ep3.entity;public class Team {
+package com.example.ewdj_ep3.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NamedQueries({})
+@Table(name = "Teams")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = { "id", "registrations", "vendors" })
+@ToString(exclude = { "id", "registrations" })
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
